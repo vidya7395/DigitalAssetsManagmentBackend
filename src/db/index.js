95a4343615c5b297074a496a1c20a86fd745c34d
Vidhya Satpute <vidhya7395@gmail.com>
2024-08-14 19:19:59 +0530

    added json vercel file

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
+import * as tedious from 'tedious';
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 1433, // Default port for SQL Server
   dialect: 'mssql',
   logging: false,
+  dialectModule: tedious,
   dialectOptions: {
     options: {
       encrypt: true,
